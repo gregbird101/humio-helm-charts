1802dade2e5d06f2a1759e9cf67c476c4091bbfc
thebirdman <gbird@mercedesamgf1.com>
2022-02-02 07:45:14 +0000

    updated with full backing

--- bin/package.sh
+++ bin/package.sh
@@ -4,24 +4,24 @@ set -ex
 
 # Fetch helm dependencies. Currently there is no way to build helm chart dependencies recursively
 # (https://github.com/helm/helm/issues/2247), so until that issue is fixed we are fetching them here
-# rm ./humio-helm-charts/charts/humio-core/charts/*.tgz 
-# helm repo add confluentinc https://humio.github.io/cp-helm-charts
-# helm repo update
-# pushd humio-helm-charts/charts/humio-core && helm dep up; popd
-# pushd humio-helm-charts/charts/humio-metrics && helm dep up; popd
+rm ./humio-helm-charts/charts/humio-core/charts/*.tgz 
+helm repo add confluentinc https://humio.github.io/cp-helm-charts
+helm repo update
+pushd humio-helm-charts/charts/humio-core && helm dep up; popd
+pushd humio-helm-charts/charts/humio-metrics && helm dep up; popd
 
-# # Remove any existing indexes
-# find . -iname index.yaml | grep -v docs | xargs rm
+# Remove any existing indexes
+find . -iname index.yaml | grep -v docs | xargs rm
 
-# # Move existing releases to the charts directory so they can be re-indexed with proper URLs
-# rm -rf /tmp/docs.save && mkdir /tmp/docs.save
-# find docs/ -name '*.tgz' -maxdepth 1 -exec mv {} /tmp/docs.save/ \;
+# Move existing releases to the charts directory so they can be re-indexed with proper URLs
+rm -rf /tmp/docs.save && mkdir /tmp/docs.save
+find docs/ -name '*.tgz' -maxdepth 1 -exec mv {} /tmp/docs.save/ \;
 
 helm package ./humio-helm-charts/
 
-# mv /tmp/docs.save/*.tgz ./ && rmdir /tmp/docs.save
+mv /tmp/docs.save/*.tgz ./ && rmdir /tmp/docs.save
 
-# helm repo index . && mv index.yaml ./docs
+helm repo index . && mv index.yaml ./docs
 
-# # Move all the charts to the docs so they can be served by github pages
-# find . -name '*.tgz' -mindepth 1 -maxdepth 1 -exec mv {} docs/ \;
+# Move all the charts to the docs so they can be served by github pages
+find . -name '*.tgz' -mindepth 1 -maxdepth 1 -exec mv {} docs/ \;
